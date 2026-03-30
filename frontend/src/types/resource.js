44733065c26f539569e4fbe26e9fa2eccab48602
SithumKaveesha1 <168362793+SithumKaveesha1@users.noreply.github.com>
2026-03-30 16:35:47 +0530

    all the resources feature

---
+++ frontend/src/types/resource.js
@@ -0,0 +1,12 @@
+export const ResourceTypes = {
+  LAB: 'LAB',
+  HALL: 'HALL',
+  EQUIPMENT: 'EQUIPMENT',
+  STUDIO: 'STUDIO',
+  OFFICE: 'OFFICE'
+};
+
+export const ResourceStatuses = {
+  ACTIVE: 'ACTIVE',
+  OUT_OF_SERVICE: 'OUT_OF_SERVICE'
+};
